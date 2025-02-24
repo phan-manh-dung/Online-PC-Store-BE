302f06e287d76c52ee0d7fc0df49fff186955ae6
Phan Mạnh Dũng <phanmanhdung2k3@gmail.com>
2025-02-24 20:45:56 +0700

    push Product_Router

--- BackEnd/src/product_service/routes/Product_Route.js
+++ BackEnd/src/product_service/routes/Product_Route.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const productController = require('../controllers/Product_Controller');
-const {  authMiddleware } = require('../middleware/authMiddleware');
+const { authMiddleware } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.get('/get-all', productController.getAllProducts);
-router.get('/get-by-id/:id',authMiddleware, productController.getProductById);
+router.get('/get-by-id/:id', productController.getProductById);
 
 router.post('/admin/create', productController.createProduct);
 router.put('/admin/update/:id', productController.updateProduct);
-router.delete('/admin/delete/:id',authMiddleware, productController.deleteProduct);
+router.delete('/admin/delete/:id', authMiddleware, productController.deleteProduct);
 
 module.exports = router;
